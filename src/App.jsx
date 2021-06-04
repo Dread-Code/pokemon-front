1f78c22451fa3c8675e48238fc58e709f0dbda7e
DreadfulCrazy <lucasospina97@gmail.com>
2021-06-04 14:17:32 -0500

    Webpack, plugins and basic loaders for react and html added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const App = () => {
-    return(
+    return (
         <div>
-            <h1>Aqui empezaria la estrutura del App</h1>
+            <h1>App...</h1>
         </div>
     )
 }
+
+export default App
