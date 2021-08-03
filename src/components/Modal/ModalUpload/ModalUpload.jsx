88d85a4a78332201e605cb1d10ffaf33a00034af
DreadfulCrazy <lucasospina97@gmail.com>
2021-08-03 18:01:18 -0500

    modal upload added

--- src/components/Modal/ModalUpload/ModalUpload.jsx
+++ src/components/Modal/ModalUpload/ModalUpload.jsx
@@ -1,16 +1,27 @@
 import React, { useCallback, useState } from 'react'
 import PropTypes from 'prop-types'
-import { Modal, Icon, Button, Dimmer, Loader } from 'semantic-ui-react'
+import { Modal, Icon, Button, Dimmer, Loader, Grid, Form } from 'semantic-ui-react'
 import { toast } from 'react-toastify'
 import { useDropzone } from 'react-dropzone'
 import { useMutation } from '@apollo/client'
-
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import POKEMON_REGISTER from '../../../gql/pokemon/pokemonRegister'
 import './ModalUpload.scss'
+
+const initialValues = () => ({
+  name: '',
+  power: '',
+  healthPoints: '',
+  pokemonType: '',
+  description: ''
+})
 
 const ModalUpload = ({ showModal, setShowModal }) => {
   const [fileUpload, setFileUpload] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
-  const [publish] = useMutation()
+  const [check, setCheck] = useState(false)
+  const [pokemonRegister] = useMutation(POKEMON_REGISTER)
 
   const onDrop = useCallback(acceptedFile => {
     setFileUpload({
@@ -32,12 +43,18 @@ const ModalUpload = ({ showModal, setShowModal }) => {
     setFileUpload(null)
     setShowModal(false)
   }
+  const handleOnCheck = () => {
+    setCheck(!check)
+  }
 
-  const onPublish = async () => {
+  const onPublish = async formData => {
+    console.log(formData)
+    console.log(fileUpload)
     try {
       setIsLoading(true)
-      const { data } = await publish({
+      const { data } = await pokemonRegister({
         variables: {
+          input: formData,
           file: fileUpload.file
         }
       })
@@ -51,32 +68,98 @@ const ModalUpload = ({ showModal, setShowModal }) => {
       console.log(error)
     }
   }
+  const { handleChange, handleSubmit, errors } = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object({
+      name: Yup.string().required('Pokemon name is require.'),
+      power: Yup.string().required('Pokemon power is required.'),
+      healthPoints: Yup.string().required('Pokemon health is require'),
+      pokemonType: Yup.string().required('Pokemon type is required'),
+      description: Yup.string()
+    }),
+    onSubmit: async formData => {
+      formData.healthPoints = parseInt(formData.healthPoints, 10)
+      formData.power = parseInt(formData.power, 10)
+
+      onPublish({ ...formData, public: check })
+    }
+  })
 
   return (
     <Modal size="small" open={showModal} onClose={onCloseModal} className="modal-upload">
-      <div {...getRootProps()} className="dropzone" style={fileUpload && { border: 0 }}>
-        {!fileUpload && (
-          <>
-            <Icon name="cloud upload" />
-            <p>Arrastra avatar del pokemon</p>
-          </>
-        )}
-        <input {...getInputProps()} />
-      </div>
-      {fileUpload?.type === 'image' && (
-        <div className="image" style={{ backgroundImage: `url("${fileUpload.preview}")` }} />
-      )}
-      {fileUpload && (
-        <Button className="btn-upload btn-action" onClick={onPublish}>
-          Publicar
-        </Button>
-      )}
-      {isLoading && (
-        <Dimmer active className="publishing">
-          <Loader />
-          <p>Publicando...</p>
-        </Dimmer>
-      )}
+      <Grid>
+        <Grid.Column width={10} className="dropzone-column">
+          <div {...getRootProps()} className="dropzone" style={fileUpload && { border: 0 }}>
+            {!fileUpload && (
+              <>
+                <Icon name="cloud upload" />
+                <p>Arrastra avatar del pokemon</p>
+              </>
+            )}
+            <input {...getInputProps()} />
+          </div>
+          {fileUpload?.type === 'image' && (
+            <div className="image" style={{ backgroundImage: `url("${fileUpload.preview}")` }} />
+          )}
+          {isLoading && (
+            <Dimmer active className="publishing">
+              <Loader />
+              <p>Publicando...</p>
+            </Dimmer>
+          )}
+        </Grid.Column>
+        <Grid.Column width={6} verticalAlign="middle" className="form-column">
+          <Form className="register-form" onSubmit={handleSubmit}>
+            <Form.Input
+              type="text"
+              placeholder="Pokemon Name"
+              name="name"
+              onChange={handleChange}
+              error={errors.name}
+            />
+            <Form.Input
+              type="text"
+              placeholder="Power"
+              name="power"
+              onChange={handleChange}
+              error={errors.power}
+            />
+            <Form.Input
+              type="text"
+              placeholder="Health Points"
+              name="healthPoints"
+              onChange={handleChange}
+              error={errors.healthPoints}
+            />
+            <Form.Input
+              type="text"
+              placeholder="Pokemon Type"
+              name="pokemonType"
+              onChange={handleChange}
+              error={errors.pokemonType}
+            />
+            <Form.Input
+              type="text"
+              placeholder="Description"
+              name="description"
+              onChange={handleChange}
+              error={errors.description}
+            />
+            <Form.Checkbox
+              toggle
+              label="Public?"
+              name="public"
+              checked={check}
+              onClick={handleOnCheck}
+            />
+            {fileUpload && (
+              <Button className="btn-submit" type="submit">
+                Create
+              </Button>
+            )}
+          </Form>
+        </Grid.Column>
+      </Grid>
     </Modal>
   )
 }
